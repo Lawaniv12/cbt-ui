3e1851e1b874154f4c45d01dd85a51ec6ca9d24e
SUN DEVELOPER - Victor Emmanuel <developer@sun.edu.ng>
2023-09-19 14:21:15 +0100

    updates to system

---
+++ src/polyfills.ts
@@ -0,0 +1,14 @@
+import '@angular/localize/init';
+
+import 'zone.js';
+
+
+import 'intl';
+
+import 'core-js/es7/reflect';
+
+import 'core-js';
+
+import 'core-js/es7/reflect';
+
+import 'zone.js/dist/zone';
